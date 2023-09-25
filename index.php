201ba689134e2648b64e3b93c012752767d448ee
Raian Ruku <raianruku21@gmail.com>
2023-09-25 21:16:41 +0600

    movie details ready

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <style>
-  <?php include 'CSS/index.css'; ?>
+  <?php include "/CSE482/CSS/index.css" ?>
 </style>
 
 <!DOCTYPE html>
@@ -9,7 +9,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="/CSS/index.css" />
+  <link rel="stylesheet" href="/CSE482/CSS/index.css" />
 </head>
 
 <body>
@@ -20,9 +20,7 @@
       <div class="search-bar">
         <input type="text" name="search" id="search" placeholder="search" />
         <button type="submit">
-          <button type="submit">
-            <ion-icon name="search-outline"></ion-icon>
-          </button>
+          <ion-icon name="search-outline"></ion-icon>
         </button>
       </div>
     </form>
@@ -38,27 +36,24 @@
       <button>View All</button>
     </div>
     <br />
-
-
     <div class="trendingcards">
-      <a href="/CSE482/pages/movie_details.php?movie_id=' . $row['id'] . '">
-        <?php
-        require_once "database.php";
-        $sql = "SELECT * FROM movies";
-        $result = mysqli_query($con, $sql);
+      <?php
+      require_once "database.php";
+      $sql = "SELECT * FROM movies";
+      $result = mysqli_query($con, $sql);
 
-        if ($result && mysqli_num_rows($result) > 0) {
-          while ($row = mysqli_fetch_assoc($result)) {
+      if ($result && mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+          $movieName = $row['movie_name'];
+          $actors = $row['cast'];
+          $director = $row['director'];
+          $category = $row['category'];
+          $releaseDate = $row['release_date'];
+          $rating = $row['rating'];
 
-            $movieName = $row['movie_name'];
-            $actors = $row['cast'];
-            $director = $row['director'];
-            $category = $row['category'];
-            $releaseDate = $row['release_date'];
-            $rating = $row['rating'];
-
-
-            echo '<div class="trending-card">
+          // Create a link for each movie card
+          echo '<a href="/CSE482/pages/movie_details.php?movie_id=' . $row['id'] . '">';
+          echo '<div class="trending-card">
                   <img src="/CSS/IMG_20230427_183213.png" alt="trending movie" />
                   <div class="trending-card-info">
                     <h3>' . $movieName . '</h3>
@@ -69,16 +64,15 @@
                     <p>' . $rating . '</p>
                   </div>
                 </div>';
-          }
-        } else {
-          echo 'No movies found.';
+          echo '</a>';
         }
-        ?>
+      } else {
+        echo 'No movies found.';
+      }
+      ?>
     </div>
   </div>
-  </div>
   <div class="vertical-line"></div>
-
   <script src="/CSE482/JS/logo.js"></script>
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
--- pages/movie_details.php
+++ pages/movie_details.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -57,6 +57,103 @@
         <p>Reputation</p>
       </div>
     </div>
+  </div>
+
+  <script src="/JS/logo.js"></script>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="/CSE482/CSS/movie_details.css" />
+</head>
+
+<body>
+  <div class="top-panel">
+    <a href="/pages/index.html" data-value="FLIXDB" id="logo">FLIXDB</a>
+    <ion-icon name="menu-outline" id="hb"></ion-icon>
+    <form action="">
+      <div class="search-bar">
+        <input type="text" name="search" id="search" placeholder="search" />
+        <button type="submit">
+          <ion-icon name="search-outline"></ion-icon>
+        </button>
+      </div>
+    </form>
+    <div class="user-icons">
+      <a href="/CSE482/pages/profile_landing.html"><ion-icon name="person-outline"></ion-icon></a>
+      <ion-icon name="bookmark-outline"></ion-icon>
+      <a href="/pages/signin.html"><ion-icon name="log-in-outline"></ion-icon></a>
+    </div>
+  </div>
+
+  <div class="movie">
+    <?php
+    // Check if movie_id is set in the URL
+    if (isset($_GET['movie_id'])) {
+      $movieId = $_GET['movie_id'];
+      require_once "database.php"; // Include your database connection code
+
+      // Prepare and execute a SQL query to fetch the movie details
+      $sql = "SELECT * FROM movies WHERE id = ?";
+      $stmt = mysqli_prepare($con, $sql);
+
+      if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "i", $movieId);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+
+        if ($result && mysqli_num_rows($result) > 0) {
+          $row = mysqli_fetch_assoc($result);
+          $movieName = $row['movie_name'];
+          $rating = $row['rating'];
+          $popularity = $row['popularity'];
+          $releaseDate = $row['release_date'];
+          $category = $row['category'];
+          $stars = $row['cast'];
+          $director = $row['director'];
+          $details = $row['details'];
+
+          echo '<div class="sd">
+                            <p>' . $movieName . '</p>
+                            <div class="rp">
+                                <p>Rating: ' . $rating . '</p>
+                                <p>Popularity: ' . $popularity . '</p>
+                            </div>
+                        </div>
+                        <p>Release Date: ' . $releaseDate . '</p>
+                        <img src="/CSS/IMG_20230427_183213.png" alt="images here with carousel" />
+                        <p>Category: ' . $category . '</p>
+                        <p>Stars: ' . $stars . '</p>
+                        <p>Director: ' . $director . '</p>
+                        <p>Details: ' . $details . '</p>';
+        } else {
+          echo 'Movie not found.';
+        }
+
+        mysqli_stmt_close($stmt);
+      } else {
+        echo 'SQL statement preparation failed.';
+      }
+
+      mysqli_close($con);
+    } else {
+      echo 'Movie ID not provided.';
+    }
+    ?>
+  </div>
+
+  <div class="reviews">
+    <p>User Reviews</p>
+    <!-- Add code to display user reviews here if needed -->
   </div>
 
   <script src="/JS/logo.js"></script>
